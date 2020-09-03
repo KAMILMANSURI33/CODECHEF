18ca63dc854cc36483e96ed8cffd7f726d095835
KAMILMANSURI33 <70717619+KAMILMANSURI33@users.noreply.github.com>
2020-09-04 01:08:05 +0530

    Update MULTHREE.cpp

--- MULTHREE.cpp
+++ MULTHREE.cpp
@@ -8,62 +8,55 @@ int main()
 	cin >> T_case;
 	for(int i = 0; i < T_case; i++)
 	{
-		ll K;
-		cin >> K;
-		ll a[K];
-		ll c,b;
-		cin >> a[0] >> a[1];
-		ll sum = (a[0] + a[1])%10;
-//		cout << a[0] << endl << a[1] << endl;
-		a[2] = 2*(sum % 10);
-		if(a[2] == 6)
-		{
-			sum = sum + ((K-3)/4)*20 + a[2];
-			ll temp = (K-3) % 4;
-			int list[] = {2,4,8,6};
-			for(int i = 0; i < temp;i++)
+		ll k;
+		cin >> k;
+		ll a,b;
+		cin >> a >> b;
+		ll ans = (a+b)%10;
+		if(k == 2){
+			if(ans % 3 ==0 ){
+				printf("YES\n");
+			} 
+			else 
 			{
-				sum += list[i];
+				printf("NO\n");
 			}
+			continue;
 		}
-		else if(a[2] == 2)
+		ans += (a+b)%10;
+		k -= 3;
+		ans += (2 * (a+b)%10) * (k / 4) ;
+		ans += (4 * (a+b)%10) * (k / 4) ;
+		ans += (8 * (a+b)%10) * (k / 4) ;
+		ans += (6 * (a+b)%10) * (k / 4) ;
+		k %= 4;
+		if(k > 0)
 		{
-			sum = sum + ((K-3)/4)*20 + a[2];
-			ll temp = (K-3) % 4;
-			int list[] = {4,8,6,2};
-			for(int i = 0; i < temp;i++)
-			{
-				sum += list[i];
-			}
+			ans += 2 * (a+b) % 10 ;
+			k--;
 		}
-		else if(a[2] == 4)
+		if(k > 0)
 		{
-			sum = sum + ((K-3)/4)*20 + a[2];
-			ll temp = (K-3) % 4;
-			int list[] = {8,6,2,4};
-			for(int i = 0; i < temp;i++)
-			{
-				sum += list[i];
-			}
+			ans += 4 * (a+b) % 10 ;
+			k--;
 		}
-		else if(a[2] == 8)
+		if(k > 0)
 		{
-			sum = sum + ((K-3)/4)*20 + a[2];
-			ll temp = (K-3) % 4;
-			int list[] = {6,2,4,8};
-			for(int i = 0; i < temp;i++)
-			{
-				sum += list[i];
-			}
+			ans += 8 * (a+b) % 10 ;
+			k--;
 		}
-		if(sum % 3 == 0)
+		if(k > 0)
 		{
-			cout << "YES" << endl;
+			ans += 6 * (a+b) % 10 ;
+			k--;
+		}
+		if(ans % 3 ==0 )
+		{
+			printf("YES\n");
 		}
 		else
 		{
-			cout << "NO" << endl;
+			printf("NO\n");
 		}
 	}
-	return 0;
 }
