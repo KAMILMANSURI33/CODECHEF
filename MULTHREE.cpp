0f3144e9364f54c04d5aa58eba7db18f620305a4
KAMILMANSURI33 <70717619+KAMILMANSURI33@users.noreply.github.com>
2020-09-03 23:21:03 +0530

    Add files via upload

---
+++ MULTHREE.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+	int T_case;
+	cin >> T_case;
+	for(int i = 0; i < T_case; i++)
+	{
+		ll K;
+		cin >> K;
+		ll a[K];
+		ll c,b;
+		cin >> a[0] >> a[1];
+		ll sum = (a[0] + a[1]);
+//		cout << a[0] << endl << a[1] << endl;
+		a[2] = 2*(sum % 10);
+		if(a[2] == 2)
+		{
+			sum = sum + (K/4)*20;
+			ll temp = K % 4;
+			int list[] = {2,4,8,6};
+			for(int i = 0; i < temp;i++)
+			{
+				sum += list[i];
+			}
+		}
+		else if(a[2] == 4)
+		{
+			sum = sum + (K/4)*20;
+			ll temp = K % 4;
+			int list[] = {4,8,6,2};
+			for(int i = 0; i < temp;i++)
+			{
+				sum += list[i];
+			}
+		}
+		else if(a[2] == 8)
+		{
+			sum = sum + (K/4)*20;
+			ll temp = K % 4;
+			int list[] = {8,6,2,4};
+			for(int i = 0; i < temp;i++)
+			{
+				sum += list[i];
+			}
+		}
+		else if(a[2] == 6)
+		{
+			sum = sum + (K/4)*20;
+			ll temp = K % 4;
+			int list[] = {6,2,4,8};
+			for(int i = 0; i < temp;i++)
+			{
+				sum += list[i];
+			}
+		}
+		if(sum % 3 == 0)
+		{
+			cout << "YES" << endl;
+		}
+		else
+		{
+			cout << "NO" << endl;
+		}
+	}
+	return 0;
+}
